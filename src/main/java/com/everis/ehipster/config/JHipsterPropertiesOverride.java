ba1ee30579ee38ad1dd94ce096881ebed771de13
joewhite101 <j.white@entando.com>
2018-07-12 17:22:41 +0200

    Add some base props

---
+++ src/main/java/com/everis/ehipster/config/JHipsterPropertiesOverride.java
@@ -0,0 +1,8 @@
+package com.everis.ehipster.config;
+
+import io.github.jhipster.config.JHipsterProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JHipsterPropertiesOverride extends JHipsterProperties{
+}
